5bf63fa6fc59017ff04eae5d8585f5edec690fcf
Steven Edwards <winehacker@gmail.com>
2023-08-23 22:05:58 -0700

    Save point

--- tests/full-sim.c
+++ tests/full-sim.c
@@ -29,7 +29,6 @@ struct _BroadwayServer {
 
   guint process_input_idle;
   GList *incomming;
-
 };
 
 struct _BroadwayServerClass
@@ -42,6 +41,9 @@ const gchar *display = ":0";   // This is lazy
 			       
 // Global server var, still loook for better way to do this
 GdkBroadwayServer *server;        // We could change this back to GdkBroadwayServer and save a headache
+
+#define WIDTH 700
+#define HEIGHT 500
 
 void init_broadway_connection(void)
 {
@@ -92,15 +94,14 @@ void test_broadway_display_open (const char *display)
   broadway_display->default_screen = broadway_display->screens[0];
   printf("Default Screen in place\n");
 
-  //broadway_device_manager = wine_broadway_device_manager_new ((char) display);
-
-  //gtk_init (display);
-
-  //printf("Broadway Device Manager Initialized: %d\n", (const char) broadway_device_manager);
-
+  /* There is no DND support
+   * this is a stub interface to a stub class 
+   */
   //wine_broadway_display_init_dnd (*display);
 
+  /* FIXME: Check why the specific screen fails here */
   //wine_broadway_screen_setup (broadway_display->screens[0]);
+
   wine_broadway_screen_setup (broadway_display->default_screen);
   printf("Screen setup for default_screen\n");
 
@@ -109,7 +110,7 @@ void test_broadway_display_open (const char *display)
     printf("broadway_display->default_screen == NULL - Trying to check BROADWAY_DISPLAY\n");
     printf(" This should never happen\n");
 
-    //display_env_var g_getenv("BROADWAY_DISPLAY");
+    //display_env_var = g_getenv("BROADWAY_DISPLAY");
     //broadway_display->screens = display_env_var;
     broadway_display->default_screen = g_getenv("BROADWAY_DISPLAY");
     test_broadway_display_open(display);
@@ -132,11 +133,60 @@ void test_broadway_display_open (const char *display)
   }
 }
 
+void test_broadway_window(GdkBroadwayDisplay *broadway_display)
+    gunit32 *id;
+    cairo_surface_t *surface;
+
+    id = wine_broadway_server_new_window(server,0,0,100,100,0);
+    printf("wine_broadway_server_new_window function did something\n");
+
+    wine_broadway_server_window_show (server, id);
+    printf("wine_broadway_server_window_show function did something\n");
+
+    surface = wine_broadway_server_create_surface(100,100);
+    printf("wine_broadway_server_create_surface did something\n");
+
+    wine_broadway_server_window_update (server, id, surface);
+    printf("wine_broadway_server_window_update\n");
+
+    wine_broadway_server_window_show (server, id);
+    printf("wine_broadway_server_window_show again\n");
+}
+
 void main()
 {
    //gtk_init();
 
+   struct GdkBroadwayDisplay *broadway_display;
+
    init_broadway_connection();
-   test_broadway_display_open(display);
+   broadway_display = test_broadway_display_open(display);
+   test_broadway_window(GdkBroadwayDisplay *broadway_display);
 }
 
+#if 0
+int create_window_surface(cairo_t *cr) {
+  /* Surface for each rendering of the PNG file, in effect, ever pass becomes a frame */
+  cairo_surface_t* surface =  cairo_image_surface_create_from_png("moving.png");
+
+  cairo_set_source_surface (cr,surface, 0, 0);
+
+  cairo_paint (cr);
+
+  //cairo_t *cr = cairo_create (surface);
+  cairo_status_t retm1;
+
+  // Fill the entire surface with red.
+  cairo_set_source_rgb(cr, 1, 0, 0);
+  cairo_rectangle(cr, 0, 0, WIDTH, HEIGHT);
+  cairo_fill(cr);
+}
+#endif
+
+#if 0
+/* We are so not here yet */
+void test_kbd_and_mouse()
+{
+  //broadway_device_manager = wine_broadway_device_manager_new ((char) display);
+  //printf("Broadway Device Manager Initialized: %d\n", (const char) broadway_device_manager);
+}
